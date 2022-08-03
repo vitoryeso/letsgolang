926bea67bae4f874315a3cc963b0c7fb5a2f8c79
vitoryeso <vitoryeso@outlook.com>
2022-08-03 11:38:31 -0300

    add a config structure and use static-dir parameter

--- src/web/lu.go
+++ src/web/lu.go
@@ -6,11 +6,24 @@ import (
 	"net/http"
 )
 
+type Config struct {
+    Addr        string
+    StaticDir   string
+}
+
 func main() {
     /* getting args configuration parameters
      * setting addr flag. */
+    /*
     addr := flag.String("addr", "localhost:4000", "HTTP Network Address")
     flag.Parse() // Getting the value for addr variable. Now addr points to the real value
+    */
+
+    cfg := new(Config)
+    flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP Network Address")
+    flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to the static files directory")
+    flag.Parse()
+
 
     mux := http.NewServeMux()
 
@@ -22,15 +35,15 @@ func main() {
     mux.HandleFunc("/user", objectQuery)
 
     // creating fileserver which deals 
-    fileServer := http.FileServer(http.Dir("./ui/static"))
+    fileServer := http.FileServer(http.Dir(cfg.StaticDir))
     // set mux to handle with /static prefix
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
     // Start listen on :4000
     // addr is a pointer to the network address value. We need to dereference
     // it using * operator
-    log.Printf("Start listening on port %s\n", *addr)
-    err := http.ListenAndServe(*addr, mux)
+    log.Printf("Start listening on port %s\n", cfg.Addr)
+    err := http.ListenAndServe(cfg.Addr, mux)
 
     // call os.exit(1) immediately after writing the message
     log.Fatal(err)
